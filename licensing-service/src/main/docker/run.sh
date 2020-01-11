b31068f3172b843dc58f5a34a85f9886cf8fc176
Saeed Kayvanfar <kayvanfar.sj@gmail.com>
2020-01-11 23:31:03 +0330

    Configuring the licensing service to use Spring Cloud Config(fixed)

--- licensing-service/src/main/docker/run.sh
+++ licensing-service/src/main/docker/run.sh
@@ -4,15 +4,18 @@ echo "********************************************************"
 echo "Waiting for the configuration server to start on port $CONFIGSERVER_PORT"
 echo "********************************************************"
 while ! `nc -z configserver $CONFIGSERVER_PORT `; do sleep 3; done
+#while ! `nc -z 127.0.0.1 $CONFIGSERVER_PORT `; do sleep 3; done
 echo ">>>>>>>>>>>> Configuration Server has started"
 
 echo "********************************************************"
 echo "Waiting for the database server to start on port $DATABASESERVER_PORT"
 echo "********************************************************"
 while ! `nc -z database $DATABASESERVER_PORT`; do sleep 3; done
+#while ! `nc -z 127.0.0.1 $DATABASESERVER_PORT`; do sleep 3; done
 echo ">>>>>>>>>>>> Database Server has started"
 
 echo "********************************************************"
 echo "Starting License Server with Configuration Service :  $CONFIGSERVER_URI";
 echo "********************************************************"
+echo "***** $CONFIGSERVER_URI 8888 $PROFILE"
 java -Dspring.cloud.config.uri=$CONFIGSERVER_URI -Dspring.profiles.active=$PROFILE -jar /usr/local/licensingservice/@project.build.finalName@.jar
