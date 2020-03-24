876038cf499d14948df8084eefe54deb6dd1ea97
Saeed Kayvanfar <kayvanfar.sj@gmail.com>
2020-03-25 01:53:26 +0430

    Consuming JavaScript Web Tokens in your microservices

--- licensing-service/src/main/java/ir/sk/eagleeye/licenses/config/ServiceConfig.java
+++ licensing-service/src/main/java/ir/sk/eagleeye/licenses/config/ServiceConfig.java
@@ -9,7 +9,29 @@ public class ServiceConfig{
   @Value("${example.property}")
   private String exampleProperty;
 
+  @Value("${redis.server}")
+  private String redisServer="";
+
+  @Value("${redis.port}")
+  private String redisPort="";
+
+  @Value("${signing.key}")
+  private String jwtSigningKey="";
+
   public String getExampleProperty(){
     return exampleProperty;
   }
+
+  public String getJwtSigningKey() {
+    return jwtSigningKey;
+  }
+
+  public String getRedisServer(){
+    return redisServer;
+  }
+
+  public Integer getRedisPort(){
+    return new Integer( redisPort ).intValue();
+  }
+
 }
