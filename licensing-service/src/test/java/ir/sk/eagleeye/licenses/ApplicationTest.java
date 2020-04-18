85a4c80bc0c33d7408f9398304e7aa4aafb1b0dc
Saeed Kayvanfar <kayvanfar.sj@gmail.com>
2020-04-18 18:49:20 +0430

    spring-boot-starter-test is added

---
+++ licensing-service/src/test/java/ir/sk/eagleeye/licenses/ApplicationTest.java
@@ -0,0 +1,24 @@
+package ir.sk.eagleeye.licenses;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+/**
+ * @author <a href="kayvanfar.sj@gmail.com">Saeed Kayvanfar</a> on 4/18/2020.
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = Application.class)
+@WebAppConfiguration
+public class ApplicationTest {
+
+    /**
+     * Test that the context loads
+     */
+    @Test
+    public void contextLoads() {
+    }
+}
