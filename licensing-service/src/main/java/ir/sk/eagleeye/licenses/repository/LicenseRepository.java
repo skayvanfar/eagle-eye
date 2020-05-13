b7ecf6a0f64cfb51d190876b3632b21a0dadd47a
Saeed Kayvanfar <kayvanfar.sj@gmail.com>
2020-05-14 02:13:12 +0430

    improvements

--- licensing-service/src/main/java/ir/sk/eagleeye/licenses/repository/LicenseRepository.java
+++ licensing-service/src/main/java/ir/sk/eagleeye/licenses/repository/LicenseRepository.java
@@ -8,7 +8,7 @@ import java.util.List;
 
 @Repository
 public interface LicenseRepository extends CrudRepository<License, String> {
-    public List<License> findByOrganizationId(String organizationId);
+    List<License> findByOrganizationId(String organizationId);
 
-    public License findByOrganizationIdAndLicenseId(String organizationId, String licenseId);
+    License findByOrganizationIdAndLicenseId(String organizationId, String licenseId);
 }
--- organization-service/src/main/java/ir/sk/eagleeye/organization/repository/OrganizationRepository.java
+++ organization-service/src/main/java/ir/sk/eagleeye/organization/repository/OrganizationRepository.java
@@ -6,5 +6,7 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface OrganizationRepository extends CrudRepository<Organization, String> {
-    public Organization findById(String organizationId);
+
+    // @Query("Organization o where o.organizationId='Seattle'")
+    Organization findById(String organizationId);
 }
